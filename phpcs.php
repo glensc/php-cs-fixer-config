ffd4c4a2acce0aad08c8ff7de7d287f1328d36f1
Elan Ruusamäe <glen@delfi.ee>
2016-10-08 04:01:19 +0300

    Update autoload to work from vendor

--- phpcs.php
+++ phpcs.php
@@ -1,5 +1,10 @@
 <?php
 
-require_once __DIR__ . '/vendor/autoload.php';
+// installed via composer?
+if (file_exists($autoload = __DIR__ . '/../../autoload.php')) {
+	require_once $autoload;
+} else {
+	require_once __DIR__ . '/vendor/autoload.php';
+}
 
 return new ED\CS\Config\ED();
