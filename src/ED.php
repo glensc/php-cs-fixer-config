ad874643e2a1fa20dbc3a1f645b43be2725c8a12
Elan Ruusamäe <glen@delfi.ee>
2016-11-09 13:33:17 +0200

    Move Git related code to GitHelper
    better overview and possibility for unit testing

--- src/ED.php
+++ src/ED.php
@@ -2,12 +2,7 @@
 
 namespace ED\CS\Config;
 
-use Symfony\Component\Finder\SplFileInfo;
-use Symfony\Component\Process\Exception\ProcessFailedException;
-use Symfony\Component\Process\Process;
 use Symfony\CS\Config\Config;
-use Symfony\CS\Finder\DefaultFinder;
-use Symfony\CS\FinderInterface;
 use Symfony\CS\FixerInterface;
 
 class ED extends Config {
@@ -21,7 +16,8 @@ class ED extends Config {
 
 		$finder = $this->getFinder();
 
-		$this->setupGit($finder);
+		$gitHelper = new Helper\GitHelper($finder);
+		$gitHelper->findGitFiles();
 
 		// revert *.xml from DefaultFinder
 		$finder
@@ -32,52 +28,6 @@ class ED extends Config {
 		$finder
 			->ignoreDotFiles(false)
 			->name('.php_cs');
-	}
-
-	/**
-	 * Setup Finder to inspect only files that are present in Git index.
-	 *
-	 * Will skip this process silently if the repository is not in git repo.
-	 *
-	 * @link https://github.com/FriendsOfPHP/PHP-CS-Fixer/issues/2214
-	 * @param FinderInterface|DefaultFinder $finder
-	 */
-	private function setupGit($finder) {
-		try {
-			$project_dir = $this->getCommandOutput("git rev-parse --show-toplevel");
-		} catch (ProcessFailedException $e) {
-			return;
-		}
-
-		if (!$project_dir) {
-			return;
-		}
-		$finder->in($project_dir);
-
-		$files = explode("\n", $this->getCommandOutput("git ls-files"));
-
-		// this filter would accept only files that are present in Git
-		$finder->filter(function (SplFileInfo $file) use (&$files) {
-			$key = array_search($file->getRelativePathname(), $files);
-
-			return $key;
-		});
-	}
-
-	/**
-	 * DRY wrapper to run program and capture it's output using Symfony Process Component
-	 *
-	 * @param string $command
-	 * @return string
-	 */
-	private function getCommandOutput($command) {
-		$process = new Process($command);
-		$process->mustRun();
-
-		$output = $process->getOutput();
-
-		// omit the final newline from output
-		return rtrim($output, PHP_EOL);
 	}
 
 	public function getRules() {
---
+++ src/Helper/GitHelper.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace ED\CS\Config\Helper;
+
+use Symfony\Component\Finder\SplFileInfo;
+use Symfony\Component\Process\Exception\ProcessFailedException;
+use Symfony\Component\Process\Process;
+use Symfony\CS\Finder\DefaultFinder;
+use Symfony\CS\FinderInterface;
+
+class GitHelper {
+	/**
+	 * @var DefaultFinder|FinderInterface
+	 */
+	private $finder;
+
+	/**
+	 * GitHelper constructor.
+	 *
+	 * @param FinderInterface|DefaultFinder $finder
+	 */
+	public function __construct($finder) {
+		$this->finder = $finder;
+	}
+
+	/**
+	 * Setup Finder to inspect only files that are present in Git index.
+	 *
+	 * Will skip this process silently if the repository is not in git repo.
+	 *
+	 * @link https://github.com/FriendsOfPHP/PHP-CS-Fixer/issues/2214
+	 */
+	public function findGitFiles() {
+		try {
+			$project_dir = $this->getCommandOutput("git rev-parse --show-toplevel");
+		} catch (ProcessFailedException $e) {
+			return;
+		}
+
+		if (!$project_dir) {
+			return;
+		}
+		$this->finder->in($project_dir);
+
+		$files = explode("\n", $this->getCommandOutput("git ls-files"));
+
+		// this filter would accept only files that are present in Git
+		$this->finder->filter(function (SplFileInfo $file) use (&$files) {
+			$key = array_search($file->getRelativePathname(), $files);
+
+			return $key;
+		});
+	}
+
+	/**
+	 * DRY wrapper to run program and capture it's output using Symfony Process Component
+	 *
+	 * @param string $command
+	 * @return string
+	 */
+	private function getCommandOutput($command) {
+		$process = new Process($command);
+		$process->mustRun();
+
+		$output = $process->getOutput();
+
+		// omit the final newline from output
+		return rtrim($output, PHP_EOL);
+	}
+}
