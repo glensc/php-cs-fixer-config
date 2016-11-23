1603aa161ab9fcd70fd5a534cea47450b5bd4c61
Elan Ruusamäe <glen@delfi.ee>
2016-11-23 11:44:29 +0200

    Setup phpunit

---
+++ tests/TestCase.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace ED\CS\Config\Test;
+
+use PHPUnit_Framework_TestCase;
+
+abstract class TestCase extends PHPUnit_Framework_TestCase {
+
+}
---
+++ tests/bootstrap.php
@@ -0,0 +1,5 @@
+<?php
+
+if (file_exists($autoload = __DIR__ . '/../vendor/autoload.php')) {
+	require_once $autoload;
+}
