e1357ab2e49d1b79b2eb4cefd9c0a6a5215e39a8
Elan Ruusamäe <glen@delfi.ee>
2018-04-24 00:30:32 +0300

    Add tests for platform version

---
+++ tests/PlatformDetectTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace ED\CS\Config\Test;
+
+use ED\CS\Config\PlatformVersion;
+
+class PlatformDetectTest extends TestCase {
+	public function testComposerNoVersion() {
+		$dir = __DIR__ . '/res/composer-json-only';
+		$version = new PlatformVersion($dir);
+		$this->assertFalse($version->satisfies('^0'));
+	}
+
+	public function testComposerVersion() {
+		$dir = __DIR__ . '/res/composer-json';
+		$version = new PlatformVersion($dir);
+		$this->assertFalse($version->satisfies('^4'));
+		$this->assertTrue($version->satisfies('^5.6'));
+		$this->assertFalse($version->satisfies('^7.1'));
+	}
+
+	public function testComposerLock() {
+		$dir = __DIR__ . '/res/composer-lock';
+		$version = new PlatformVersion($dir);
+
+		$this->assertFalse($version->satisfies('^4'));
+		$this->assertTrue($version->satisfies('^5.6'));
+		$this->assertFalse($version->satisfies('^7.1'));
+	}
+}
