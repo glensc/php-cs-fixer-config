db7470c4535e0a798e54d93bb509e594ed6e10d5
Elan Ruusamäe <glen@delfi.ee>
2019-02-13 21:01:29 +0200

    Enable combine_nested_dirname for php 7.0+

--- src/Rules/PlatformRules.php
+++ src/Rules/PlatformRules.php
@@ -18,10 +18,13 @@ class PlatformRules implements RuleInterface {
 	 */
 	public function apply(RuleBuilder $builder) {
 		$php54 = $this->platform->satisfies('>=5.4');
+		$php70 = $this->platform->satisfies('>=7.0');
 
 		$builder['array_syntax'] = ['syntax' => $php54 ? 'short' : 'long'];
 
 		// "self::" accessor in closures requires php >= 5.4, not safe to always enable
 		$builder['self_accessor'] = $php54;
+
+		$builder['combine_nested_dirname'] = $php70;
 	}
 }
